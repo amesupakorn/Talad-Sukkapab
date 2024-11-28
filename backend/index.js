dd131f4001d3313f4f690c10aa5d852277f661d8
SupakornThongaerd <supakorn642@gmail.com>
2024-11-29 00:11:13 +0700

    update database

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const pool = require('./database/db'); 
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5001;
 
+app.use(express.json());
+app.get('/test', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()'); 
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error acquiring client', err); 
+        res.status(500).send('Server Error');
+    }
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// ตัวอย่าง API
 app.get('/', (req, res) => {
     res.send('Hello from Backend!');
 });
